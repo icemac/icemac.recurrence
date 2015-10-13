9c92b557e7977075d535e1910174e5d7cd1c514c
Michael Howitz <mh@gocept.com>
2015-10-13 11:07:51 +0200

    Preparing release 1.0.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@
  Changelog
 ===========
 
-1.0.0 (unreleased)
+1.0.0 (2015-10-13)
 ==================
 
 * Extract package from `icemac.ab.calendar` for reuse in other projects.
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(*path_elements):
     return file(os.path.join(*path_elements)).read()
 
 
-version = '1.0.0.dev0'
+version = '1.0.0'
 long_description = '\n\n'.join([
     read('README.rst'),
     read('CHANGES.rst'),
